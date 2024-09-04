de8df406ad9bf5956244f1d4e3ee0e2d43818380
Md. Jahid Hasan <122719439+jahidhasanpiyesh@users.noreply.github.com>
2024-09-05 01:01:53 +0600

    Delete coreapp directory
     dsfa

--- coreapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import add_post
-# Register your models here.
-@admin.register(add_post)
-class show(admin.ModelAdmin):
-    list_display = ['title','desh']
